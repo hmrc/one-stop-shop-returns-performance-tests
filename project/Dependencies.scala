1dc0180e6dcfb6d81f801da0105179fb308b5a4c
susan-badel <susan.badel@opencastsoftware.com>
2022-01-10 13:54:05 +0000

    Updated dependencies in line with platform requirements

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,9 +6,9 @@ object Dependencies {
 
   val test = Seq(
     "com.typesafe"          % "config"                    % "1.3.1"         % Test,
-    "uk.gov.hmrc"          %% "performance-test-runner"   % "4.11.0"        % Test,
+    "uk.gov.hmrc"          %% "performance-test-runner"   % "5.2.0"         % Test,
     "io.gatling"            % "gatling-test-framework"    % gatlingVersion  % Test,
     "io.gatling.highcharts" % "gatling-charts-highcharts" % gatlingVersion  % Test,
-    "org.mongodb.scala"   %% "mongo-scala-driver" % "2.6.0"   % Test
+    "org.mongodb.scala"    %% "mongo-scala-driver"        % "2.6.0"         % Test
   )
 }
