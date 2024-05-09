ffb8923258f95852204bec86db279730e777af73
Susan Badel <susan.badel@opencastsoftware.com>
2024-05-09 10:11:38 +0100

    VEOSS-1829 updated to use stub test data

--- src/test/scala/uk/gov/hmrc/perftests/returns/ReturnsRequests.scala
+++ src/test/scala/uk/gov/hmrc/perftests/returns/ReturnsRequests.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 HM Revenue & Customs
+ * Copyright 2024 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -50,11 +50,11 @@ object ReturnsRequests extends ServicesConfiguration {
       .formParam("redirectionUrl", homepageUrl)
       .formParam("enrolment[0].name", "HMRC-MTD-VAT")
       .formParam("enrolment[0].taxIdentifier[0].name", "VRN")
-      .formParam("enrolment[0].taxIdentifier[0].value", "${vrn}")
+      .formParam("enrolment[0].taxIdentifier[0].value", "100000002")
       .formParam("enrolment[0].state", "Activated")
       .formParam("enrolment[1].name", "HMRC-OSS-ORG")
       .formParam("enrolment[1].taxIdentifier[0].name", "VRN")
-      .formParam("enrolment[1].taxIdentifier[0].value", "${vrn}")
+      .formParam("enrolment[1].taxIdentifier[0].value", "100000002")
       .formParam("enrolment[1].state", "Activated")
       .check(status.in(200, 303))
       .check(headerRegex("Set-Cookie", """mdtp=(.*)""").saveAs("mdtpCookie"))
