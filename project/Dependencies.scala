d68c860636f0ad47e68f40849498de5525ea868f
Susan Swinburne <susan.swinburne@opencastsoftware.com>
2026-05-05 10:40:29 +0100

    VEI-858 remove remaining test-only endpoint database clear down elements

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,6 @@ object Dependencies {
 
   val test = Seq(
     "com.typesafe"          % "config"                    % "1.4.4"         % Test,
-    "uk.gov.hmrc"          %% "performance-test-runner"   % "6.3.0"         % Test,
-    "org.scalaj"           %% "scalaj-http"               % "2.4.2"         % Test
+    "uk.gov.hmrc"          %% "performance-test-runner"   % "6.3.0"         % Test
   )
 }
