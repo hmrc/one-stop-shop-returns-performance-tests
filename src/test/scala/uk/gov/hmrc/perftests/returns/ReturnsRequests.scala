688d0f39b8118d521818fce40179215b1cf273ae
Susan Swinburne <susan.swinburne@opencastsoftware.com>
2026-06-17 16:38:25 +0100

    VEOSS-2192 added pause builder for file upload

--- src/test/scala/uk/gov/hmrc/perftests/returns/ReturnsRequests.scala
+++ src/test/scala/uk/gov/hmrc/perftests/returns/ReturnsRequests.scala
@@ -187,13 +187,15 @@ object ReturnsRequests extends ServicesConfiguration {
       .get("#{fileUpload}")
       .check(status.in(200))
 
-  def postFileUploaded(period: String) =
+  def postFileUploaded(period: String) = {
+    pause(5000)
     http("Post File Uploaded page")
       .post("#{fileUpload}")
       .formParam("csrfToken", "#{csrfToken}")
       .formParam("value", true)
       .check(status.in(200, 303))
       .check(header("Location").is(s"$route/$period/correct-previous-return"))
+  }
 
   def getSoldGoodsFromNi(period: String) =
     http("Get Sold Goods From Ni page")
